d6681a90e97fee613f710df496d3568b80343fc7
Luca Del Puppo <Puppo@users.noreply.github.com>
2025-06-26 10:43:01 +0200

    chore: review tstyche test (#207)
    * chore: review tstyche test
    
    * refactor: types test
    
    * refactor: change arrow functions to regular functions in test setup
    
    * refactor: change import to type for Orama types
    
    * refactor: update tsconfig and remove unused tstyche config
    
    * refactor: simplify typescript script and remove tstyche config

--- test/types/index.tst.ts
+++ test/types/index.tst.ts
@@ -1,87 +1,121 @@
-import { expect, test } from 'tstyche'
+import { describe, expect, test } from 'tstyche'
 
-import { InternalTypedDocument, Orama, PartialSchemaDeep, Results, Schema, SearchParams, TypedDocument } from '@orama/orama'
+import type { InternalTypedDocument, Orama, PartialSchemaDeep, Results, Schema, SearchParams, TypedDocument } from '@orama/orama'
 import Fastify from 'fastify'
 import fp from 'fastify-plugin'
 
-import { PersistenceInFile, PersistenceInMemory, fastifyOrama } from '../../index.js'
-
-const app = Fastify()
+import { OramaApi, PersistenceInFile, PersistenceInMemory, fastifyOrama } from '../../index.js'
 
 const mySchema = {
   quote: 'string',
   author: 'string'
 } as const
+
 type MySchema = Schema<typeof mySchema>
 
+function baseSetup() {
+  const app = Fastify()
 
-app.register(fastifyOrama, {
-  schema: mySchema,
-  prefix: '/api/orama',
-  language: 'en'
-})
-
-app.register(fastifyOrama, {
-  persistence: new PersistenceInMemory()
-})
-
-app.register(fastifyOrama, {
-  persistence: new PersistenceInMemory({
-    jsonIndex: 'index.json'
+  app.register(fastifyOrama, {
+    schema: mySchema,
+    persistence: new PersistenceInMemory()
   })
-})
 
-app.register(fastifyOrama, {
-  persistence: new PersistenceInFile()
-})
-app.register(fastifyOrama, {
-  persistence: new PersistenceInFile({
-    filePath: 'index.json',
-    format: 'json',
-    mustExistOnStart: true
+  return app.withOrama<typeof mySchema>()
+}
+
+function setupWithMorePersistenceOptions() {
+  const app = baseSetup()
+
+  app.register(fastifyOrama, {
+    schema: mySchema,
+    prefix: '/api/orama',
+    language: 'en'
   })
-})
 
-const appWithOrama = app.withOrama<typeof mySchema>()
-
-test('should enable the insertion of documents', () => {
-  appWithOrama.orama.insert({ quote: 'Hello', author: 'World' }).then(id => {
-    expect(id).type.toBe<string>()
+  app.register(fastifyOrama, {
+    persistence: new PersistenceInMemory({
+      jsonIndex: 'index.json'
+    })
   })
-})
 
-test('should enable the searching of documents', () => {
-  appWithOrama.get('/hello', async () => {
-
-    const {orama} = appWithOrama
-    const result = await orama.search({ term: 'hello' })
-
-    expect(result).type.toBe<Results<InternalTypedDocument<MySchema>>>()
-    expect(result.hits[0].document.author).type.toBe<string>()
-
-    return {
-      hello: result.hits
-    }
+  app.register(fastifyOrama, {
+    persistence: new PersistenceInFile()
   })
-})
+  app.register(fastifyOrama, {
+    persistence: new PersistenceInFile({
+      filePath: 'index.json',
+      format: 'json',
+      mustExistOnStart: true
+    })
+  })
 
-test('should expose the Orama API', () => {
-  expect(appWithOrama.orama).type.toBe<{
-    insert: (document: PartialSchemaDeep<TypedDocument<Orama<typeof mySchema>>>) => Promise<string>,
-    search: (params: SearchParams<Orama<Schema<typeof mySchema>>, typeof mySchema>) => Promise<Results<Schema<typeof mySchema>>>,
-    persist?: () => Promise<any>,
-  }>()
-})
+  return app.withOrama<typeof mySchema>()
+}
 
+describe('Fastify Orama', () => {
 
-test('should enable the withOrama method', () => {
-  fp(function(fastify) {
-    const fastifyWithOrama = fastify.withOrama<typeof mySchema>()
+  test('should expose the Orama instance', async () => {
+    await (async function () {
+      const appWithOrama = baseSetup()
+      const id = await appWithOrama.orama.insert({ quote: 'Hello', author: 'World' })
+      expect(id).type.toBe<string>()
 
-    expect(fastifyWithOrama.orama).type.toBe<{
+      appWithOrama.get('/hello', async () => {
+
+        const {orama} = appWithOrama
+        expect(orama).type.toBe<OramaApi<typeof mySchema>>()
+        const result = await orama.search({ term: 'hello' })
+
+        return {
+          hello: result.hits
+        }
+      })
+    })()
+  });
+
+  test('should enable the insertion of documents', () => {
+    const appWithOrama = setupWithMorePersistenceOptions()
+    appWithOrama.orama.insert({ quote: 'Hello', author: 'World' }).then(id => {
+      expect(id).type.toBe<string>()
+    })
+  })
+
+  test('should enable the searching of documents', () => {
+    const appWithOrama = setupWithMorePersistenceOptions()
+    appWithOrama.get('/hello', async () => {
+
+      const {orama} = appWithOrama
+      const result = await orama.search({ term: 'hello' })
+
+      expect(result).type.toBe<Results<InternalTypedDocument<MySchema>>>()
+      expect(result.hits[0].document.author).type.toBe<string>()
+
+      return {
+        hello: result.hits
+      }
+    })
+  })
+
+  test('should expose the Orama API', () => {
+    const appWithOrama = setupWithMorePersistenceOptions()
+    expect(appWithOrama.orama).type.toBe<{
       insert: (document: PartialSchemaDeep<TypedDocument<Orama<typeof mySchema>>>) => Promise<string>,
       search: (params: SearchParams<Orama<Schema<typeof mySchema>>, typeof mySchema>) => Promise<Results<Schema<typeof mySchema>>>,
       persist?: () => Promise<any>,
     }>()
+  })
+
+
+  test('should enable the withOrama method', () => {
+    fp(function(fastify) {
+      const fastifyWithOrama = fastify.withOrama<typeof mySchema>()
+
+      expect(fastifyWithOrama.orama).type.toBe<{
+        insert: (document: PartialSchemaDeep<TypedDocument<Orama<typeof mySchema>>>) => Promise<string>,
+        search: (params: SearchParams<Orama<Schema<typeof mySchema>>, typeof mySchema>) => Promise<Results<Schema<typeof mySchema>>>,
+        persist?: () => Promise<any>,
+      }>()
+    })
   })
 })
